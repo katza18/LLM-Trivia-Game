c24f4bc5d9573e0696ddba75dc6fda56437dc26e
Aidan Katz <akatz42@gatech.edu>
2025-02-16 11:50:43 -0500

    Update styling

--- trivia-app/src/App.tsx
+++ trivia-app/src/App.tsx
@@ -7,29 +7,40 @@ import { Input } from "./components/ui/input";
 function App() {
   return (
     <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-    <div className="min-h-screen bg-background text-primary flex flex-col items-center justify-center">
-      <div className="bg-card p-4 w-100 flex justify-end">
-        <ModeToggle />
+    <div className="min-h-screen bg-background text-primary flex flex-col p-4">
+      <div className="header grid grid-cols-3 gap-4 h-auto">
+        <div className="title col-span-1 col-start-2 flex justify-center bg-card">
+          <p>Quiz Me!</p>
+        </div>
+        <div className="theme col-start-3 flex justify-end">
+          <ModeToggle />
+        </div>
       </div>
-      <div className="bg-card p-4 w-full flex flex-col items-center justify-center">
-        <h1>Quiz Me!</h1>
-      </div>
-      <div className="App-body">
-        <h2>--- Question Number will go here ---</h2>
-        <p>Question will go here</p>
-        <Button variant='outline'>Answer 1</Button>
-        <Button variant='outline'>Answer 2</Button>
-        <Button variant='outline'>Answer 3</Button>
-        <Button variant='outline'>Answer 4</Button>
-        <Input type="text" placeholder="Enter your answer here" />
-        <Button variant='secondary'>Submit Answer</Button>
+      <div className="timer flex items-center justify-center flex-grow">
         <p>Time left: 15 seconds</p>
-        <p>Correct!</p>
-        <p>Incorrect! The correct answer was: Answer 2</p>
-        <Button variant='secondary'>Submit question correction</Button> {/* This should open a form to be filled out that submits a correction to the questions/answers */}
       </div>
-      {/* QA Style ask the user what they'd like to be quizzed on */}
-      {/* <Chat /> */}
+      <div className="body grid grid-cols-3 gap-4 h-auto flex-grow">
+        <div className="quiz col-start-2 flex flex-col justify-center bg-card gap-2">
+          <h2 className="text-center">--- Question # ---</h2>
+          <p className='text-center'>Question?</p>
+          <Button variant='outline'>Answer 1</Button>
+          <Button variant='outline'>Answer 2</Button>
+          <Button variant='outline'>Answer 3</Button>
+          <Button variant='outline'>Answer 4</Button>
+          <Input className="hidden" type="text" placeholder="Enter your answer here" />
+          <Button variant='default'>Submit Answer</Button>
+          <p className="hidden">Correct!</p>
+          <p className="hidden">Incorrect! The correct answer was: Answer 2</p>
+        </div>
+      </div>
+      <div className="score flex justify-center items-center flex-grow ">
+        <p>100%</p>
+      </div>
+      <div className="footer grid grid-cols-3 gap-4 h-auto">
+        <div className="correction col-start-2 flex justify-center">
+          <Button variant='outline'>Submit question correction</Button> {/* This should open a form to be filled out that submits a correction to the questions/answers */}
+        </div>
+      </div>
     </div>
     </ThemeProvider>
   )
