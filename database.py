b8e80f57f994d9d43f27f3cc029685e4309e0bdf
Aidan Work <aidankatz1835@gmail.com>
2025-04-21 14:24:14 -0400

    update database.py

--- database.py
+++ database.py
@@ -26,8 +26,28 @@ def initialize_database():
             question TEXT,
             type TEXT,
             topic TEXT,
-            answer TEXT,
+            answer TEXT
         )
     ''')
     conn.commit()
     conn.close()
+
+
+def get_answer(question_id: str, question_type: str) -> str:
+        # Check the answer in the database
+        conn = sqlite3.connect(DATABASE_NAME)
+        cursor = conn.cursor()
+
+        if question_type == 'multi':
+            table_name = MULTI_CHOICE_TABLE_NAME
+        else:
+            table_name = SHORT_ANSWER_TABLE_NAME
+        
+        cursor.execute(f'SELECT answer FROM {table_name} WHERE id=?', (question_id,))
+        correct_answer = cursor.fetchone()
+
+        if correct_answer:
+            correct_answer = correct_answer[0]
+            return correct_answer
+        else:
+            return None
