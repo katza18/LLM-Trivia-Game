2a9a1b35da95395d7b8f3423ffcf837ac85cbe91
Aidan Katz <akatz42@gatech.edu>
2025-04-30 12:59:24 -0400

    Add supertokens dependency

--- backend/api/questions.py
+++ backend/api/questions.py
@@ -2,18 +2,22 @@ from fastapi import APIRouter, Depends, HTTPException
 from openai import AsyncOpenAI
 from backend.quiz import generate_quiz, QuizRequest
 from backend.crud import question
+from supertokens_python.recipe.session.framework.fastapi import verify_session
+from supertokens_python.recipe.session import SessionContainer
 
 router = APIRouter(prefix="/questions", tags=["questions"])
 
 
 @router.post("/generate")
-async def generate_quiz(quiz_params: QuizRequest, client: AsyncOpenAI = Depends()):
+async def generate(quiz_params: QuizRequest, openai_client: AsyncOpenAI = Depends(), session: SessionContainer = Depends(verify_session())):
     """
     Endpoint to generate a quiz.
     TODO: Add SQL injection prevention and input validation.
     """
+    # Check that this user has enough quota left
+
     try:
-        quiz_data = await generate_quiz(quiz_params, client)
+        quiz_data = await generate_quiz(quiz_params, openai_client)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
