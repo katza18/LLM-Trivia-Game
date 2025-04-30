471e3ba74606dc0f3e51f74082631a5c679542ea
Aidan Katz <akatz42@gatech.edu>
2025-04-30 12:53:52 -0400

    move db files to db folder

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "sqlite:///./quizgen.db"
+DATABASE_URL = f'postgresql://{os.getenv('QUIZGEN_DB_USER')}:{os.getenv('QUIZGEN_DB_PASSWORD')}@{os.getenv('QUIZGEN_DB_URI')}'
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
