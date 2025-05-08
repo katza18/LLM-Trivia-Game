435b3643438856be607608113e80ddf8abbbd158
Aidan Work <aidankatz1835@gmail.com>
2025-05-08 15:05:27 -0400

    remove user pay tier and refactor token naming

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
 from backend.db.database import Base
 
 class User(Base):
@@ -7,7 +8,7 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String, unique=True)
     email = Column(String, unique=True)
-    pay_tier = Column(String, default='free')
-    created_at = Column(DateTime)
-    token_used = Column(Integer, default=0)
-    quota_expiry = Column(DateTime)
+    created_at = Column(DateTime, default=func.now())
+    tokens_used = Column(Integer, default=0)
+    quota = Column(Integer, default=0)
+    quota_expiration = Column(DateTime)
