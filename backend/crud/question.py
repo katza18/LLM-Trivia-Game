b5840fb23bf406abace39f6dba883e29dbf54cc0
Aidan Work <aidankatz1835@gmail.com>
2025-05-08 15:03:46 -0400

    Add user recent questions get function

--- backend/crud/question.py
+++ backend/crud/question.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import select
 from pydantic import BaseModel
 from backend.models.question import Question as QuestionModel
+from backend.models.view import View as ViewModel
 
 class QuestionSchema(BaseModel):
     id: int = None
@@ -15,30 +16,43 @@ class QuizSchema(BaseModel):
     questions: list[QuestionSchema]
 
 
-def get_answer(session: Session, question_id: int) -> str:
+def get_answer(db: Session, question_id: int) -> str:
     # Check the answer in the database and return
     statement = select(QuestionModel.answer).where(QuestionModel.id == question_id)
-    answer = session.execute(statement).scalars().one()
+    answer = db.execute(statement).scalars().one()
     return answer
 
 
-def get_previous_questions(session: Session, topic: str) -> list[str]:
+def get_previous_questions(db: Session, topic: str) -> list[str]:
     # Get previous questions from the database
     statement = select(QuestionModel.question).where(QuestionModel.topic == topic)
-    questions = session.execute(statement).scalars().all()
+    questions = db.execute(statement).scalars().all()
     return questions
 
+def get_user_previous_questions(db: Session, user_id: str, quantity: int):
+    # Get previous questions for a specific user from the database sorted by created_at
+    recent_views_statement = (
+        select(ViewModel)
+        .where(ViewModel.user_id == user_id)
+        .order_by(ViewModel.last_viewed.desc())
+        .limit(quantity))
+    statement = (
+        select(QuestionModel)
+        .join(recent_views_statement.subquery(), QuestionModel.id == recent_views_statement.c.question_id)
+    )
+    questions = db.execute(statement).scalars().all()
+    return questions
 
-def save_quiz(session: Session, quiz_data: QuizSchema) -> list[QuestionSchema]:
+def save_quiz(db: Session, quiz_data: QuizSchema) -> list[QuestionSchema]:
     # Convert the questions to Question objects
     questions = [QuestionSchema(**q.dict()) for q in quiz_data]
 
     # Add the questions to the session and commit
-    session.add_all(questions)
-    session.commit()
+    db.add_all(questions)
+    db.commit()
 
     # Refresh the session to get the IDs of the new questions
-    session.refresh(question for question in questions)
+    db.refresh(question for question in questions)
 
     # Return the list of questions with their IDs
     return questions
